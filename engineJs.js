42298f6ea33b744e25b110a448fb27bd240eacb5
milkywoosh <archer.lukman@gmail.com>
2021-07-14 23:21:00 +0700

    add hovering mouse on element
     - using onmouseover
    - using onmouseout
    - add asyncronous to make UI better look

--- engineJs.js
+++ engineJs.js
@@ -1,14 +1,57 @@
-const Suit = require("./oop-suit");
+// const Suit = require("./oop-suit");
 
 // input (gunting, kertas, batu);
 
-const game = new Suit();
-
-// console.log( game.suit("gunting"));
+class SuitClass {
 
 
+    suit(inputUser) {
+        let pool = ["gunting", "batu", "kertas"];
+        let rand = Math.floor(Math.random() * pool.length);
+        let result;
+        if (inputUser == "gunting") {
+            if (pool[rand] == inputUser) {
+                result = "seri";
+            } else if (pool[rand] == "kertas") {
+                result = "menang"
+            } else if (pool[rand] == "batu") {
+                result = "kalah"
+            }
+        } else if (inputUser == "batu") {
+            if (pool[rand] == inputUser) {
+                result = "seri";
+            } else if (pool[rand] == "kertas") {
+                result = "kalah"
+            } else if (pool[rand] == "gunting") {
+                result = "menang"
+            }
+        } else if (inputUser == "kertas") {
+            if (pool[rand] == inputUser) {
+                result = "seri";
+            } else if (pool[rand] == "gunting") {
+                result = "kalah"
+            } else if (pool[rand] == "batu") {
+                result = "menang"
+            }
+        }
+        return result;
+    }
+}
 
-const batu = document.getElementById("batu-player1");
-const gunting = document.getElementById("gunting-player1");
-const kertas = document.getElementById("kertas-player1");
+// class Suit
+let suit = new SuitClass();
 
+
+/* ON OFF hovering element
+    onmouseover = "getId(this)"
+    onmouseout = "getOutId(this)"
+*/
+function getId (obj) {
+    document.getElementById(obj.id).classList.add("set-bg-grey")
+}
+function getOutId(obj) {
+    setTimeout( () => {
+        document.getElementById(obj.id).classList.remove("set-bg-grey")
+     }, 500 );
+   
+}
